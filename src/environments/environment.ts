2abc35f36fa8afa913f4556f54e320de50c1379d
Alberto Basalo <albertobasalo@hotmail.com>
2021-09-07 13:11:42 +0200

    fix: environmet property name

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  // apiHost: 'http://localhost:3000/v1/',
-  apihost: 'https://proton-angular-builders.herokuapp.com/v1/',
+  apiHost: 'https://proton-angular-builders.herokuapp.com/v1/',
 };
 
 /*
