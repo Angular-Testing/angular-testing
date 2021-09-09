bd8a624a5f6b0a6936bd9a521b1c3ecbcfa565ff
Alberto Basalo <albertobasalo@hotmail.com>
2021-09-09 18:42:47 +0200

    test: video 1.1 sut doc spy

--- src/app/core/layout/confirm-modal/confirm-modal.component.spec.ts
+++ src/app/core/layout/confirm-modal/confirm-modal.component.spec.ts
@@ -1,24 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ConfirmStore } from '@ab/global/confirm.store';
 import { ConfirmModalComponent } from './confirm-modal.component';
 
-describe('ConfirmModalComponent', () => {
-  let component: ConfirmModalComponent;
-  let fixture: ComponentFixture<ConfirmModalComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ConfirmModalComponent],
-    }).compileComponents();
-  });
-
+fdescribe('GIVEN: a ConfirmModalComponent with a ConfirmStore', () => {
+  let sut: ConfirmModalComponent;
+  let doc: ConfirmStore;
   beforeEach(() => {
-    fixture = TestBed.createComponent(ConfirmModalComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    // Arrange
+    doc = jasmine.createSpyObj('ConfirmStore', ['confirm', 'getState$']); // doble del ConfirmStore
+    sut = new ConfirmModalComponent(doc);
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('WHEN call the "onConfirmClick" method', () => {
+    beforeEach(() => {
+      // Act
+      sut.onConfirmClick();
+    });
+    it('THEN should call store.confirm method', () => {
+      // Assert
+      expect(doc.confirm).toHaveBeenCalled();
+    });
   });
 });
