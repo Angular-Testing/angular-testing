44c1df41c609a0caaf33a80a1993d688eb96ede4
Alberto Basalo <albertobasalo@hotmail.com>
2021-09-29 13:46:49 +0200

    test: video 3.3 host container

--- src/app/shared/ui/card/card.component.spec.ts
+++ src/app/shared/ui/card/card.component.spec.ts
@@ -1,6 +1,6 @@
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { CardComponent } from './card.component';
+import { Card, CardComponent } from './card.component';
 
 describe('CardComponent', () => {
   let component: CardComponent;
@@ -20,5 +20,60 @@ describe('CardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+});
+@Component({
+  template: '<ab-card [card]="card" (actionClick)="onActionClick($event)" ></ab-card>',
+})
+class CardHostComponent {
+  public card!: Card;
+  constructor() {}
+  public onActionClick(action: string) {}
+}
+
+fdescribe('GIVEN the CardComponent on a Host component', () => {
+  let hostComponent: CardHostComponent;
+  let hostFixture: ComponentFixture<CardHostComponent>;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CardComponent, CardHostComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    hostFixture = TestBed.createComponent(CardHostComponent);
+    hostComponent = hostFixture.componentInstance;
+    hostFixture.detectChanges();
+  });
+
+  describe('WHEN receives an input card', () => {
+    beforeEach(() => {
+      // Act
+      hostComponent.card = { title: 'title', actions: [] };
+      hostFixture.detectChanges();
+    });
+    it('THEN should display the caption', () => {
+      // Assert
+      const actual = hostFixture.nativeElement.querySelector('.card-header-title').textContent;
+      const expected = hostComponent.card.title;
+      expect(actual).toEqual(expected);
+    });
+  });
+  describe('WHEN users clicks on a button', () => {
+    beforeEach(() => {
+      // Arrange
+      spyOn(hostComponent, 'onActionClick');
+      hostComponent.card = {
+        title: 'title',
+        actions: [{ caption: 'Click Me', command: 'clickCommand' }],
+      };
+      hostFixture.detectChanges();
+      // Act
+      hostFixture.nativeElement.querySelector('[data-testid="command-button"]').click();
+    });
+    it('THEN should emit the event', () => {
+      // Assert
+      expect(hostComponent.onActionClick).toHaveBeenCalled();
+    });
   });
 });
