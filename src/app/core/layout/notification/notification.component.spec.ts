7cdb466e1673b8be9c1d8c9df4aecdacce86829c
Alberto Basalo <albertobasalo@hotmail.com>
2021-09-29 14:37:49 +0200

    chore: pre 3.3

--- src/app/core/layout/notification/notification.component.spec.ts
+++ src/app/core/layout/notification/notification.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NotificationComponent } from './notification.component';
 
 describe('NotificationComponent', () => {
@@ -22,3 +21,33 @@ describe('NotificationComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+// @Pipe({
+//   name: 'status',
+// })
+// class FakeSatusPipe implements PipeTransform {
+//   transform(value: string): string {
+//     return value;
+//   }
+//   constructor() {}
+// }
+
+// fdescribe('GIVEN the NotificationComponent', () => {
+//   let component: NotificationComponent;
+//   let fixture: ComponentFixture<NotificationComponent>;
+//   describe('WHEN the store sends a notification', () => {
+//     beforeEach(async () => {
+//       await TestBed.configureTestingModule({
+//         declarations: [NotificationComponent],
+//       }).compileComponents();
+//     });
+
+//     beforeEach(() => {
+//       fixture = TestBed.createComponent(NotificationComponent);
+//       component = fixture.componentInstance;
+//       fixture.detectChanges();
+//     });
+//     it('THEN should display the notification', () => {});
+//     it('AND THEN should call the close method after 3 seconds', fakeAsync(() => {}));
+//   });
+// });
